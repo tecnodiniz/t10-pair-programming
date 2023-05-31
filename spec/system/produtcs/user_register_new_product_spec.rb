9e7d4f50049bdd619c749174142e3e7c9075b2f7
Eduardo Diniz <diniz480@gmail.com>
2023-05-31 15:49:18 -0300

    Cadastro produto

---
+++ spec/system/produtcs/user_register_new_product_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'usuário cadastra um produto' do 
+    context "a partir da tela inicial" do
+        it "com sucesso" do
+
+            visit root_path 
+            click_on 'Cadastrar produto'
+
+            fill_in "Nome",	with: "Monitor Gamer"
+            fill_in "Código", with: "MOG XPDR"
+            fill_in "Altura", with: "20"
+            fill_in "Peso", with: "100"
+
+            click_on "Cadastrar"
+
+            expect(current_path).to eq root_path
+            expect(page).to have_content "Monitor Gamer"
+            expect(page).to have_content "MOG XPDR"
+            expect(page).to have_content "20"
+            expect(page).to have_content "100"
+            
+        end
+    end
+    
+end
